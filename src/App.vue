bc8f6663c92b38a1fa53f312ffdd0e67e3aac9e9
Sabrina_Rashid <saashid18@gmail.com>
2025-05-25 19:14:16 +0600

    home and navbar done

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
+import Navbar from '@/components/Navbar.vue';
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  Job POrtal
+  <Navbar />
+  <RouterView />
 </template>
-
